485d0947d556fbb6ade56039eb53e7bc4844cc68
Deepanshu9643 <deepanshukhorwal7@gmail.com>
2024-09-17 12:17:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ let input2 = prompt("Enter the second number:");
 	if(isNaN(no1) || isNaN(no2)){
 		alert("Invalid input. Please enter a valid number.")
 	}
+		if(input1 === "" || input2 === ""){
+		alert("Invalid input. Please enter a valid number.")
+		return
+	}
 	else{
 		const sum = no1 + no2
 		alert(`the sum of ${input1} and ${input2} is ${sum}` )
